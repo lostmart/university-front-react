db7a656e2e04f58818c39d9e3a78dcd3d0d3f466
lostmart <martinpedraza1979@gmail.com>
2024-01-25 20:15:06 +0100

    new swipper css rules

--- src/components/ui/SwipperCarrousel.jsx
+++ src/components/ui/SwipperCarrousel.jsx
@@ -1,7 +1,14 @@
+import { NavLink } from "react-router-dom"
+
+import styles from "./Swipper.module.scss"
+
 // slides
 import slideOne from "../../assets/img/uni-01.jpg"
 import slideTwo from "../../assets/img/uni-2.jpeg"
 import slideThree from "../../assets/img/uni-03.jpeg"
+import slideFour from "../../assets/img/uni-04.jpeg"
+import slideFive from "../../assets/img/uni-05.webp"
+import slideSix from "../../assets/img/uni-06.webp"
 
 import { Swiper, SwiperSlide } from "swiper/react"
 
@@ -13,17 +20,82 @@ import "swiper/css/navigation"
 import { Navigation } from "swiper/modules"
 
 const SwipperCarrousel = () => {
-	const slides = [slideOne, slideTwo, slideThree]
+	const slides = [
+		{
+			imgUrl: slideOne,
+			title: "Breaking Boundaries in Education",
+			description: "Your Journey Begins at Horizon University",
+		},
+		{
+			imgUrl: slideTwo,
+			title: "Empowering Minds, Inspiring Futures",
+			description: "Your Journey Begins at Horizon University",
+		},
+		{
+			imgUrl: slideThree,
+			title: "Do somee stuff, then do some other",
+			description: "Do it now or never at Horizon University",
+		},
+		{
+			imgUrl: slideFour,
+			title: "Breaking Boundaries in Education",
+			description: "Your Journey Begins at Horizon University",
+		},
+		{
+			imgUrl: slideFive,
+			title: "Empowering Minds, Inspiring Futures",
+			description: "Your Journey Begins at Horizon University",
+		},
+		{
+			imgUrl: slideSix,
+			title: "Empowering Minds, Inspiring Futures",
+			description: "Your Journey Begins at Horizon University",
+		},
+	]
 	return (
-		<Swiper navigation={true} modules={[Navigation]} className="mySwiper">
-			{slides.map((slide) => {
+		<Swiper
+			slidesPerView={1}
+			spaceBetween={20}
+			centeredSlides={true}
+			loop={true}
+			pagination={{
+				clickable: true,
+			}}
+			breakpoints={{
+				640: {
+					slidesPerView: 2,
+					spaceBetween: 20,
+				},
+				1024: {
+					slidesPerView: 3,
+					spaceBetween: 50,
+				},
+			}}
+			navigation={true}
+			modules={[Navigation]}
+			className="mySwiper"
+		>
+			{slides.map((slide, i) => {
 				return (
-					<SwiperSlide>
-						<img
-							src={slide}
-							alt="horizon university"
-							style={{ objectFit: "cover", width: "100%" }}
-						/>
+					<SwiperSlide key={i}>
+						<figure className={styles.figure}>
+							<img
+								src={slide.imgUrl}
+								alt={slide.title}
+								className={styles.swiperImg}
+							/>
+							<figcaption className={styles.caption}>
+								<h2>{slide.title}</h2>
+								<p>{slide.description}</p>
+								<NavLink
+									to="/"
+									className="btn btn-primary-outline"
+									style={{ width: "82px" }}
+								>
+									Login
+								</NavLink>
+							</figcaption>
+						</figure>
 					</SwiperSlide>
 				)
 			})}
