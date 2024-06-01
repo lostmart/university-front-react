05dafbf6a84b0b4b16e61bf7c78e78d5f4e6d9da
lostmart <martinpedraza1979@gmail.com>
2024-06-01 12:23:32 +0200

    about and home page remake

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,28 @@
+import React from "react"
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom"
+import Home from "./pages/HomePage"
+import About from "./pages/AboutPage"
+
 import "./styles.scss"
 
 function App() {
 	return (
-		<>
-			<h2>here it is</h2>
-		</>
+		<Router>
+			<nav>
+				<ul>
+					<li>
+						<Link to="/">Home</Link>
+					</li>
+					<li>
+						<Link to="/about">About</Link>
+					</li>
+				</ul>
+			</nav>
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/about" element={<About />} />
+			</Routes>
+		</Router>
 	)
 }
 
