d3e11509613e0033c74fc8aeefc29a71b76ab7cd
lostmart <martinpedraza1979@gmail.com>
2025-11-11 18:43:06 +0100

    new installes

--- src/hooks/authService.ts
+++ src/hooks/authService.ts
@@ -25,7 +25,7 @@ type UserError = {
 }
 
 const authUser = async (token: string): Promise<TFullUser | UserError> => {
-	const API_AUTH_USER = env.VITE_API_AUTH_USER
+	const API_AUTH_USER = import.meta.env.VITE_API_AUTH_USER
 
 	try {
 		const res = await fetch(API_AUTH_USER, {
